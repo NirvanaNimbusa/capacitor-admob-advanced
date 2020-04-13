688f43c94b374a7b4472bffe3fcebfe121d97283
Elliot Archer <elliot@devutex.com>
2020-04-13 02:12:54 +0100

    iOS Admob sdk initialized

--- example/src/app/services/ads.service.ts
+++ example/src/app/services/ads.service.ts
@@ -39,7 +39,7 @@ export class AdsService {
     public initialise() {
         AdmobAdvanced.initializeWithConsent({
             appIdAndroid: 'ca-app-pub-3572449953921317~8063185404', // replace with your actual Android app ID
-            appIdIos: 'ca-app-pub-3940256099942544~3347511713',     // replace with your actual iOS app ID
+            appIdIos: 'ca-app-pub-3572449953921317~7887408669',     // replace with your actual iOS app ID
             publisherId: 'pub-3572449953921317',                     // replace with your actual publisher ID
             tagUnderAgeOfConsent: false
         }).then(data => {
