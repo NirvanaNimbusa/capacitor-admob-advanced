7b7959e54085b0d8f8bb6026b3b26773aefca2b7
Elliot Archer <elliot@devutex.com>
2020-04-14 02:44:09 +0100

    Removed initialise

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -16,13 +16,13 @@ public class AdmobAdvanced: CAPPlugin, GADBannerViewDelegate, GADInterstitialDel
     
     @objc func initialize(_ call: CAPPluginCall) {
         let appId = call.getString("appIdIos") ?? "ca-app-pub-6564742920318187~7217030993"
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        //GADMobileAds.sharedInstance().start(completionHandler: nil)
         call.success([ "value": appId ])
     }
     
     @objc func initializeWithConsent(_ call: CAPPluginCall) {
         let appId = call.getString("appIdIos") ?? "ca-app-pub-6564742920318187~7217030993"
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        //GADMobileAds.sharedInstance().start(completionHandler: nil)
         let pubId = call.getString("publisherId") ?? "pub-0123456789012345"
         PACConsentInformation.sharedInstance.requestConsentInfoUpdate(forPublisherIdentifiers: [pubId]) {(_ error: Error?) -> Void in
             if let error = error {
